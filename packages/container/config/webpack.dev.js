fb024549ae25caaa25a6dc71c60bad99a6a1209f
Kirill Panov <kirpa8604@gmail.com>
2023-04-17 22:17:17 -0500

    Created container frploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common');
 const packagesJson = require('../package.json');
