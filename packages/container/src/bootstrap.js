9b5d35a8b53e36aaf97efc3f907c2d740a60d421
Kirill Panov <kirpa8604@gmail.com>
2023-04-19 23:35:29 -0500

    Created container deploy script

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDomClient from 'react-dom/client';
 import App from './App';
 
+// render() is not supported, need to use createRoot
 ReactDomClient.createRoot(document.getElementById('root')).render(
         <App />
     );
