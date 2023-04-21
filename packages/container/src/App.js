e5c10ae0f3a589d9ff036e54eff1b9ca2a65aa62
Kirill Panov <kirpa8604@gmail.com>
2023-04-20 19:33:45 -0500

    Adding header to the container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import MarketingApp from "./components/marketingApp";
+import Header from './components/Header'
 
 const App = () => {
     return (
         <div>
-            <h1>Hi there!</h1>
+            <Header />
             <hr />
             <MarketingApp />
         </div>
